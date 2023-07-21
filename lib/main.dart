533c40d5823e2b500fade660b91c594ec081f344
Laura Perttilä <laura@perttila.me>
2023-07-21 12:35:37 +0300

    navigate to another page after clicking recipe list item

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,99 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// class RecipeList extends StatelessWidget {
+//   const RecipeList({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Recipes'),
+//       ),
+//       body: ListView.builder(
+//           padding: const EdgeInsets.all(8),
+//           itemCount: recipes.length,
+//           itemBuilder: (BuildContext context, int index) {
+//             return TextButton(
+//               onPressed: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => const RecipeView2(),
+//                   ),
+//                 );
+//               },
+//               child: Container(
+//                 height: 50,
+//                 color: Colors.amber[600],
+//                 child: Center(child: Text(recipes[index].name)),
+//               ),
+//             );
+//           }),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.note),
+//             label: 'Recipe',
+//           ),
+//         ],
+//         currentIndex: 0,
+//         selectedItemColor: Colors.yellow[700],
+//         unselectedItemColor: Colors.grey,
+//         showUnselectedLabels: true,
+//         onTap: (int index) {
+//           // Add your onTap code here!
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+// class RecipeView2 extends StatelessWidget {
+//   const RecipeView2({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Recipe'),
+//       ),
+//       body: const RecipeList(),
+//       bottomNavigationBar: BottomNavigationBar(
+//         items: const <BottomNavigationBarItem>[
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.note),
+//             label: 'Recipe',
+//           ),
+//         ],
+//         currentIndex: 1,
+//         selectedItemColor: Colors.yellow[700],
+//         unselectedItemColor: Colors.grey,
+//         showUnselectedLabels: true,
+//         onTap: (int index) {
+//           // Add your onTap code here!
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {},
+//         child: const Icon(Icons.edit),
+//       ),
+//     );
+//   }
+// }
+
 class RecipeList extends StatelessWidget {
   const RecipeList({super.key});
 
@@ -31,40 +124,48 @@ class RecipeList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Recipes'),
+        title: const Text('First Route'),
       ),
       body: ListView.builder(
           padding: const EdgeInsets.all(8),
           itemCount: recipes.length,
           itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 50,
-              color: Colors.amber[600],
-              child: Center(child: Text(recipes[index].name)),
+            return TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RecipeView(),
+                  ),
+                );
+              },
+              child: Container(
+                height: 50,
+                color: Colors.amber[600],
+                child: Center(child: Text(recipes[index].name)),
+              ),
             );
           }),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.note),
-            label: 'Recipe',
-          ),
-        ],
-        currentIndex: 0,
-        selectedItemColor: Colors.yellow[700],
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
-        onTap: (int index) {
-          // Add your onTap code here!
-        },
+    );
+  }
+}
+
+class RecipeView extends StatelessWidget {
+  const RecipeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
       ),
     );
   }
