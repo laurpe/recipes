1bd2013f8a3e823f75aac195c9d979ff9d28c82e
Laura Perttilä <laura@perttila.me>
2023-10-02 12:40:43 +0300

    create database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/recipe.dart';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Future onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  final database = await openDatabase(
+    join(await getDatabasesPath(), 'recipe_database.db'),
+    onConfigure: onConfigure,
+    onCreate: (db, version) async {
+      await db.execute(
+          'CREATE TABLE recipes(id INTEGER PRIMARY KEY, name TEXT, instructions TEXT)');
+      await db.execute('''CREATE TABLE ingredients(
+            id INTEGER PRIMARY KEY, 
+            name TEXT, 
+            amount TEXT, 
+            recipeId INTEGER, 
+            FOREIGN KEY(recipeId) REFERENCES recipes(id) ON DELETE CASCADE)''');
+    },
+    version: 1,
+  );
+
+  Future<void> insertIngredient(Ingredient ingredient, int recipeId) async {
+    final db = database;
+
+    await db.insert(
+      'ingredients',
+      {...ingredient.toMap(), 'recipeId': recipeId},
+    );
+  }
+
+  Future<void> insertRecipe(Recipe recipe) async {
+    final db = database;
+
+    final recipeId = await db.insert(
+      'recipes',
+      recipe.toMap(),
+    );
+
+    for (var ingredient in recipe.ingredients) {
+      await insertIngredient(ingredient, recipeId);
+    }
+  }
+
+  final ingredient1 = Ingredient(amount: "2 kg", name: "kukkakaalia");
+  final ingredient2 = Ingredient(amount: "2 dl", name: "kermaviiliä");
+  final ingredient3 = Ingredient(amount: "1 rkl", name: "suolaa");
+
+  final newRecipe = Recipe(
+      name: "kukkakaaliwingsit",
+      instructions: "Sekoita aineet",
+      ingredients: [ingredient1, ingredient2, ingredient3]);
+
+  await insertRecipe(newRecipe);
+
   runApp(const MyApp());
 }
 
@@ -23,99 +81,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class RecipeList extends StatelessWidget {
-//   const RecipeList({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Recipes'),
-//       ),
-//       body: ListView.builder(
-//           padding: const EdgeInsets.all(8),
-//           itemCount: recipes.length,
-//           itemBuilder: (BuildContext context, int index) {
-//             return TextButton(
-//               onPressed: () {
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context) => const RecipeView2(),
-//                   ),
-//                 );
-//               },
-//               child: Container(
-//                 height: 50,
-//                 color: Colors.amber[600],
-//                 child: Center(child: Text(recipes[index].name)),
-//               ),
-//             );
-//           }),
-//       bottomNavigationBar: BottomNavigationBar(
-//         items: const <BottomNavigationBarItem>[
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.note),
-//             label: 'Recipe',
-//           ),
-//         ],
-//         currentIndex: 0,
-//         selectedItemColor: Colors.yellow[700],
-//         unselectedItemColor: Colors.grey,
-//         showUnselectedLabels: true,
-//         onTap: (int index) {
-//           // Add your onTap code here!
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {},
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
-
-// class RecipeView2 extends StatelessWidget {
-//   const RecipeView2({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Recipe'),
-//       ),
-//       body: const RecipeList(),
-//       bottomNavigationBar: BottomNavigationBar(
-//         items: const <BottomNavigationBarItem>[
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.note),
-//             label: 'Recipe',
-//           ),
-//         ],
-//         currentIndex: 1,
-//         selectedItemColor: Colors.yellow[700],
-//         unselectedItemColor: Colors.grey,
-//         showUnselectedLabels: true,
-//         onTap: (int index) {
-//           // Add your onTap code here!
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {},
-//         child: const Icon(Icons.edit),
-//       ),
-//     );
-//   }
-// }
 
 class RecipeList extends StatelessWidget {
   const RecipeList({super.key});
@@ -141,7 +106,9 @@ class RecipeList extends StatelessWidget {
               child: Container(
                 height: 50,
                 color: Colors.amber[600],
-                child: Center(child: Text(recipes[index].name)),
+                child: Center(
+                    child: Text(recipes[index].name,
+                        style: TextStyle(color: Colors.amber[50]))),
               ),
             );
           }),
@@ -192,6 +159,7 @@ final letut = Recipe(
       Ingredient(amount: "1 dl", name: "jauhoja")
     ],
     instructions: "sekoita");
+
 final kakku = Recipe(
     name: "Kakku",
     ingredients: [
--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,18 +1,52 @@
 class Ingredient {
+  int? id;
   String amount;
   String name;
 
-  Ingredient({required this.amount, required this.name});
+  Ingredient({this.id, required this.amount, required this.name});
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      'amount': amount,
+      'name': name,
+    };
+
+    if (id != null) {
+      map['id'] = id;
+    }
+
+    return map;
+  }
+
+  @override
+  String toString() {
+    return 'Ingredient{id: $id, amount: $amount, name: $name}';
+  }
 }
 
 class Recipe {
+  int? id;
   String name;
   List<Ingredient> ingredients;
   String instructions;
 
   Recipe({
+    this.id,
     required this.name,
     required this.ingredients,
     required this.instructions,
   });
+
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = {
+      'name': name,
+      'instructions': instructions,
+    };
+
+    if (id != null) {
+      map['id'] = id;
+    }
+
+    return map;
+  }
 }
