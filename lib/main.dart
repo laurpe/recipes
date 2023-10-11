1020c0639dacca7b8680a0f4924816e85874633b
Laura Perttilä <laura@perttila.me>
2023-10-11 15:32:32 +0300

    wip: edit recipe form

--- lib/main.dart
+++ lib/main.dart
@@ -324,7 +324,18 @@ class RecipeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(recipe.name), centerTitle: false, actions: [
-        IconButton(onPressed: () {}, icon: const Icon(Icons.edit))
+        IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EditRecipeView(
+                    recipe: recipe,
+                  ),
+                ),
+              );
+            },
+            icon: const Icon(Icons.edit))
       ]),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -364,5 +375,209 @@ class RecipeView extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class EditRecipeView extends StatelessWidget {
+  final Recipe recipe;
+
+  const EditRecipeView({super.key, required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('Edit recipe'),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.delete),
+            ),
+          ]),
+      body: EditRecipeForm(recipe: recipe),
+    );
+  }
+}
+
+class EditRecipeForm extends StatefulWidget {
+  final Recipe recipe;
+
+  const EditRecipeForm({super.key, required this.recipe});
+
+  @override
+  EditRecipeFormState createState() {
+    return EditRecipeFormState();
+  }
+}
+
+class EditRecipeFormState extends State<EditRecipeForm> {
+  final _formKey = GlobalKey<FormState>();
+  late String _recipeName;
+  late String _instructions;
+  late List<Ingredient> _ingredients;
+
+  @override
+  void initState() {
+    _recipeName = widget.recipe.name;
+    _instructions = widget.recipe.instructions;
+    _ingredients = widget.recipe.ingredients;
+    super.initState();
+  }
+
+  void _addIndgredient() {
+    setState(() {
+      _ingredients.add(const Ingredient(name: '', amount: ''));
+    });
+  }
+
+  Future<void> _submitData() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      final recipe = Recipe(
+        name: _recipeName,
+        instructions: _instructions,
+        ingredients: _ingredients,
+      );
+
+      try {
+        GetIt.I<DatabaseClient>().insertRecipe(recipe);
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text('Recipe saved!')));
+        _formKey.currentState!.reset();
+        Navigator.of(context).pop();
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Something went wrong! Please try again.')));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Required field';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+              ),
+              onSaved: (value) {
+                _recipeName = value!;
+              },
+              initialValue: _recipeName,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Required field';
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                labelText: 'Instructions',
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+              ),
+              minLines: 10,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+              onSaved: (value) {
+                _instructions = value!;
+              },
+              initialValue: _instructions,
+            ),
+          ),
+          Flexible(
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: _ingredients.length + 1,
+              itemBuilder: (context, index) {
+                if (index == _ingredients.length) {
+                  return ElevatedButton(
+                    onPressed: _addIndgredient,
+                    child: const Text('Add ingredient'),
+                  );
+                }
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 100,
+                        child: TextFormField(
+                          initialValue: _ingredients[index].amount,
+                          decoration: const InputDecoration(
+                            labelText: 'Amount',
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter ingredient amount';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              _ingredients[index] = Ingredient(
+                                name: _ingredients[index].name,
+                                amount: value,
+                              );
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          initialValue: _ingredients[index].name,
+                          decoration: const InputDecoration(
+                            labelText: 'Ingredient name',
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter ingredient name';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            setState(() {
+                              _ingredients[index] = Ingredient(
+                                name: value,
+                                amount: _ingredients[index].amount,
+                              );
+                            });
+                          },
+                        ),
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          setState(() {
+                            _ingredients.removeAt(index);
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          ElevatedButton(onPressed: _submitData, child: const Text('Save'))
+        ]));
   }
 }
