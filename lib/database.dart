9d62214b333d5958681dae3d6d61f2a725d39c25
Laura Perttilä <laura@perttila.me>
2024-01-16 16:27:02 +0200

    when deleting a recipe also delete rows from recipe_tags table

--- lib/database.dart
+++ lib/database.dart
@@ -47,7 +47,7 @@ class DatabaseClient {
             recipe_id INTEGER NOT NULL,
             tag_id INTEGER NOT NULL,
             PRIMARY KEY(recipe_id, tag_id),
-            FOREIGN KEY(recipe_id) REFERENCES recipes(id),
+            FOREIGN KEY(recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
             FOREIGN KEY(tag_id) REFERENCES tags(id)
             )''');
 
--- lib/seed_data.dart
+++ lib/seed_data.dart
@@ -122,7 +122,57 @@ const seedRecipes = [
         Ingredient(name: "juustokastiketta", amount: "2", unit: "dl")
       ],
       favorite: false,
-      servings: 8)
+      servings: 8),
+  Recipe(
+      name: "Sämpylät",
+      instructions:
+          "Sekoita kuivat aineet. Lisää maito ja sekoita. Pyörittele sämpylöiksi. Paista 225 asteessa 20 minuuttia.",
+      ingredients: [
+        Ingredient(name: "vehnäjauhoja", amount: "5", unit: "dl"),
+        Ingredient(name: "hiivaa", amount: "1", unit: "pss"),
+        Ingredient(name: "suolaa", amount: "1", unit: "tl"),
+        Ingredient(name: "maitoa", amount: "2", unit: "dl")
+      ],
+      favorite: false,
+      servings: 16),
+  Recipe(
+      name: "Pokebowl",
+      instructions:
+          "Keitä riisi. Kuori ja pilko kasvikset. Sekoita kastikkeen ainekset. Aseta kulhoon riisi, kasvikset ja kastike. Tarjoile.",
+      ingredients: [
+        Ingredient(name: "riisiä", amount: "2", unit: "dl"),
+        Ingredient(name: "kurkkua", amount: "1", unit: "kpl"),
+        Ingredient(name: "porkkanaa", amount: "1", unit: "kpl"),
+        Ingredient(name: "avokadoa", amount: "1", unit: "kpl"),
+        Ingredient(name: "tomaattia", amount: "1", unit: "kpl"),
+        Ingredient(name: "korianteria", amount: "1", unit: "kpl"),
+        Ingredient(name: "soijakastiketta", amount: "1", unit: "rkl"),
+        Ingredient(name: "limettimehua", amount: "1", unit: "rkl"),
+        Ingredient(name: "inkivääriä", amount: "1", unit: "tl"),
+        Ingredient(name: "valkosipulinkynttä", amount: "1", unit: "kpl"),
+        Ingredient(name: "chiliä", amount: "1", unit: "kpl"),
+        Ingredient(name: "oliiviöljyä", amount: "1", unit: "rkl"),
+        Ingredient(name: "suolaa", amount: "1", unit: "tl"),
+        Ingredient(name: "mustapippuria", amount: "1", unit: "tl")
+      ],
+      favorite: false,
+      servings: 4),
+  Recipe(
+      name: "Maa-artisokkakeitto",
+      instructions:
+          "Kuori ja pilko kasvikset. Kuullota kasviksia öljyssä. Lisää vesi ja mausteet. Keitä 20 minuuttia. Soseuta sauvasekoittimella.",
+      ingredients: [
+        Ingredient(name: "maa-artisokkaa", amount: "2", unit: "kpl"),
+        Ingredient(name: "perunaa", amount: "2", unit: "kpl"),
+        Ingredient(name: "sipulia", amount: "1", unit: "kpl"),
+        Ingredient(name: "valkosipulinkynttä", amount: "2", unit: "kpl"),
+        Ingredient(name: "öljyä", amount: "1", unit: "rkl"),
+        Ingredient(name: "vettä", amount: "1", unit: "l"),
+        Ingredient(name: "suolaa", amount: "1", unit: "tl"),
+        Ingredient(name: "mustapippuria", amount: "1", unit: "tl")
+      ],
+      favorite: false,
+      servings: 4),
 ];
 
 const seedTags = ['breakfast', 'lunch', 'dinner', 'dessert', 'snack'];
